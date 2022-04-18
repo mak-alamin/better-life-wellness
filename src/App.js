a94b1b8f485fb001646e1ebecf25247e09e5c9d5
Mak Alamin <makalamin012@gmail.com>
2022-04-18 21:35:10 +0600

    added react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About/About";
+import Checkout from "./components/Checkout/Checkout";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import NotFound from "./components/NotFound/NotFound";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="home" element={<Home></Home>} />
+        <Route path="about" element={<About></About>} />
+
+        <Route path="checkout" element={<Checkout></Checkout>} />
+
+        <Route path="login" element={<Login></Login>} />
+        <Route path="register" element={<Register></Register>} />
+
+        <Route path="*" element={<NotFound></NotFound>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHXsC3Ofyt6FdqMrdXS-FCB99-xDx8lzM",
+  authDomain: "better-life-wellness.firebaseapp.com",
+  projectId: "better-life-wellness",
+  storageBucket: "better-life-wellness.appspot.com",
+  messagingSenderId: "1078712806095",
+  appId: "1:1078712806095:web:41f2d670d1493d99e1e5ba",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
